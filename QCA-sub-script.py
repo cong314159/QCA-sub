0260f19e6b1fe472565c54cd4c20caa35b31eb11
joe <joe_cong1@baylor.edu>
2020-06-15 13:19:10 -0500

    misc

--- QCA-sub-script.py
+++ QCA-sub-script.py
@@ -5,8 +5,8 @@ import numpy as np
 
 if __name__ == '__main__':
     subHome = os.getcwd()
-    # simHome = "/home/congj/code/matlab/QCAInputSim/Simulations/ICH_WindowOfOperation_study/"
-    simHome = "/Users/joe/dev/matlab/QCAInputSim/Simulations/ICH_WindowOfOperation_study/"
+    simHome = "/home/congj/code/matlab/QCAInputSim/Simulations/ICH_WindowOfOperation_study/"
+    # simHome = "/Users/joe/dev/matlab/QCAInputSim/Simulations/ICH_WindowOfOperation_study/"
 
     kodiak = True
     job = 'calculation_visualization_nodisplay'
